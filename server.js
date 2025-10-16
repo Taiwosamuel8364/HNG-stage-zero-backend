02106744c837384c40a4248cec68f70cf446d192
Taiwosamuel8364 <taiwosamuel8364@gmail.com>
2025-10-16 07:56:31 -0700

    fix: correct CORS module import in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cat_fact = require("./fact");
 const rateLimit = require("express-rate-limit");
 require("dotenv").config();
-const cors = require(cors);
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
